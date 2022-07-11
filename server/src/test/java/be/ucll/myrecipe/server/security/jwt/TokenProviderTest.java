c3c6e4b6f331b1ddbae65845f47f7bc742303e6f
Tom Vanwyngaerden <tom.vanwyngaerden@gmail.com>
2022-07-11 15:16:17 +0200

    Add security tests

--- server/src/test/java/be/ucll/myrecipe/server/security/jwt/TokenProviderTest.java
+++ server/src/test/java/be/ucll/myrecipe/server/security/jwt/TokenProviderTest.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class TokenProviderTest {
+class TokenProviderTest {
 
     private TokenProvider tokenProvider;
 
